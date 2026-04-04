3ff83200eb49e0b6528cc3dba233cef4770c9633
mika-glitch10 <bhumikac107@gmail.com>
2026-04-05 00:33:06 +0530

    initial

--- HELLOAPP.java
+++ HELLOAPP.java
@@ -1,10 +1,26 @@
-public class UC3.java {
+public class HelloApp {
 
     public static void main(String[] args) {
 
-        String name = (args.length > 0) ? args[0] : "World";
+        String greeting;
 
-        System.out.println("Hello, " + name + "!");
+        if (args.length > 0) {
+            StringBuilder names = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                names.append(args[i]);
+
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+
+            greeting = "Hello, " + names.toString() + "!";
+        } else {
+            greeting = "Hello, World!";
+        }
+
+        System.out.println(greeting);
     }
 }
 	
